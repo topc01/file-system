37beeab0d4707ee3a7a7d00112071e8e41674b3e
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-07-06 11:40:59 -0400

    build folder

---
+++ build/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/topc/Projects/EDA/Tarea 3/main.cpp" "CMakeFiles/code.dir/main.cpp.o" "gcc" "CMakeFiles/code.dir/main.cpp.o.d"
+  "/Users/topc/Projects/EDA/Tarea 3/source/item.cpp" "CMakeFiles/code.dir/source/item.cpp.o" "gcc" "CMakeFiles/code.dir/source/item.cpp.o.d"
+  "/Users/topc/Projects/EDA/Tarea 3/source/treeSO.cpp" "CMakeFiles/code.dir/source/treeSO.cpp.o" "gcc" "CMakeFiles/code.dir/source/treeSO.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/main.cpp.o"
+  "CMakeFiles/code.dir/main.cpp.o.d"
+  "CMakeFiles/code.dir/source/item.cpp.o"
+  "CMakeFiles/code.dir/source/item.cpp.o.d"
+  "CMakeFiles/code.dir/source/treeSO.cpp.o"
+  "CMakeFiles/code.dir/source/treeSO.cpp.o.d"
+  "code"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
